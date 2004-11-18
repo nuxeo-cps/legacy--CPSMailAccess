f2ab72674260562123c3d8fbf626709bbbbe5e91
fguillaume <devnull@localhost>
2004-11-18 18:14:58 +0000

    rename

--- mailfolder.py
+++ mailfolder.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2004 Nuxeo SARL <http://nuxeo.com>
-#!/usr/bin/python
-# -*- encoding: iso-8859-15 -*-
-# Author: Tarek Ziadé <tz@nuxeo.com>
+# -*- coding: ISO-8859-15 -*-
+# Copyright (c) 2004 Nuxeo SARL <http://nuxeo.com>
+# Authors: Tarek Ziadé <tz@nuxeo.com>
+#          Florent Guillaume <tz@nuxeo.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as published
@@ -17,19 +17,34 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
 #
-from interfaces import ICSPMailFolder
+# $Id$
+"""MailFolder
+
+A MailFolder contains mail messages and other mail folders.
+"""
+
 from zope.interface import implements
 from OFS.Folder import Folder
 
-class CSPMailFolder(Folder):
-    """ a Mailfolder that can contain messages and folders
+from interfaces import IMailFolder
+
+
+class MailFolder(Folder):
+    """A container of mail messages and other mail folders.
+
+    A MailFolder implements IMailFolder:
+
+    >>> f = MailFolder()
+    >>> IMailFolder.providedBy(f)
+    True
 
     """
-    
-    implements(ICSPMailFolder)
-    
-    def getMessageList(self, recursive=False):
-        """ 
-        >>> tester = CSPMailFolder()        
+
+    implements(IMailFolder)
+
+    def getMailMessages(self, recursive=False):
+        """See interfaces.IMailFolder.
+
+
         """
         return []
