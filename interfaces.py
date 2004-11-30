1dc73d2fc30eb29a9a871c2a9c9591f18f2e0202
tziade <devnull@localhost>
2004-11-30 22:22:22 +0000

    added a few elements to interfaces

--- interfaces.py
+++ interfaces.py
@@ -20,29 +20,96 @@
 from zope.interface import Interface
 from zope.app.container.interfaces import IContained, IContainer
 from zope.app.container.constraints import ContainerTypesConstraint
-from zope.schema import Field
+from zope.schema import Field, Text
 
 
 class IMailFolder(IContainer):
     """A container of mail messages and other mail folders.
     """
+    server_name = Field(
+        title=u'Server Name',
+        description=u'Corresponding server name',
+        default=u'',
+        required=False)
+        
+    mail_prefix = Field(
+        title=u'Prefix for mail objects',
+        description=u'All mails are prefixed with this',
+        default=u'',
+        required=False)        
+        
+    def getMailMessages(list_folder=True, list_messages= True, recursive=False):
+        """Retrieves all contained messages and/or folders
 
-    def getMailMessages(recursive=False):
-        """Retrieves all contained messages.
-
-        Returns a sequence of IMailMessage.
+        Returns a sequence of IMailMessage and/or IMailFolder.
         """
-
-#class IMailFolderContained(IContained):
-#    """Objects contained in MailFolders should implement this interface.
-#    """
-#    __parent__ = Field(
-#        constraint=ContainerTypesConstraint(IMailFolder))
-
-
+        
+    def getMailMessagesCount(recursive=False):
+        """ Retrieves a count of all contained messages        
+        
+        """
+        
+    def checkMessages():
+        """ Each Mail Folder can resync its content
+            according to server's folder
+            a global resync would be a resync done on 
+            the root mail folder
+        """
+        
+    def getServerName():
+        """" returns server name
+        """
+        
+    def setServerName(self, server_name):        
+        """" sets server name
+             can also hold a synchronization call
+             when the name changes
+        """
+        
+        
+    def _addMessage(uid='', msg_key=''):
+        """ Mail Message factory for this
+            MailFolder, adds an IMailMessage
+            to it and returns it
+        """
+        
+    def _addFolder(uid='', server_name=''):
+        """ Mail Folder factory for this
+            MailFolder, adds an IMailFolder
+            to it and returns it
+        """        
+        
+    def findMessage(msg_key, recursive=True):
+        """ finds a message by its msg_key
+        """ 
+        
+    def childFoldersCount():
+        """ returns the number of child fodlers
+        """           
+            
 class IMailMessage(IContainer):
     """A mail message.
 
     It stores the content of a message, this content can be composed of
     headers, ID, and optionnaly the body.
     """
+    msg_uid = Field(
+        title=u'Message ID',
+        description=u'Unique identifier for message in its container',
+        default=u'',
+        required=True)
+        
+    msg_key = Field(
+        title=u'Message Key',
+        description=u'Hash key used to identify message',
+        default=u'',
+        required=False)
+        
+        
+class IMailMessageStore(IContainer):
+    """ Provides interface to a mail message store
+        
+    """                
+    def loadMessage(raw_msg):
+        """ loads a message from a raw content (typically a string)
+        """
