5ab01ef1e3b22b843eab0597173f439c01423655
tziade <devnull@localhost>
2004-12-07 09:21:23 +0000

    added mailbox intialization for functionnal tests

--- __init__.py
+++ __init__.py
@@ -1,1 +1,29 @@
-# Make this a module
+#!/usr/bin/python
+# -*- encoding: iso-8859-15 -*-
+# (C) Copyright 2003 Nuxeo SARL <http://nuxeo.com>
+# Author: Tarek Ziadé <tz@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+import mailbox
+
+def initialize(context):
+
+    context.registerClass(
+        mailbox.MailBox,
+        constructors = (mailbox.manage_addMailBoxForm,
+                        mailbox.manage_addMailBox),
+        )
+
