67ad54fa4e7dc393ce7dcc590800d76a058cbe03
tziade <devnull@localhost>
2004-12-13 12:24:16 +0000

    renders empty body for empty messages

--- mailrenderer.py
+++ mailrenderer.py
@@ -7,10 +7,8 @@ from email.MIMEMultipart import MIMEMultipart
 from email import Iterators
 from interfaces import IMailRenderer
 from zope.interface import implements
-
 from mimetools import decode
 from email import Parser
-
 
 class MailRenderer:
     """A tool to render MIME parts
@@ -18,16 +16,18 @@ class MailRenderer:
     >>> f = MailRenderer()
     >>> IMailRenderer.providedBy(f)
     True
-
     """
     implements(IMailRenderer)
 
     def _extractBodies(self, mail):
         """ extracts the body
         """
-        it = Iterators.body_line_iterator(mail)
-        lines = list(it)
-        return ''.join(lines)
+        if mail is not None:
+            it = Iterators.body_line_iterator(mail)
+            lines = list(it)
+            return ''.join(lines)
+        else:
+            return ''
 
     def renderBody(self, mail, part_index=0):
         """ renders the mail given body part
@@ -35,8 +35,9 @@ class MailRenderer:
         if mail is None:
             return ''
 
-        if not mail.isMultipart():
-            body = self._extractBodies(mail.store)
+        if not mail.isMultipart() and part_index>0:
+            part = mail.getPart(part_index)
+            body = self._extractBodies(part)
         else:
             body = self._extractBodies(mail.getPart(part_index))
 
