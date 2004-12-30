c5a907eb0f748fd94a953c7b6dc72e1a677a71f2
tziade <devnull@localhost>
2004-12-30 16:52:03 +0000

    added synchro

--- mailbox.py
+++ mailbox.py
@@ -21,6 +21,7 @@
 
 A MailBox is the root MailFolder for a given mail account
 """
+from zLOG import LOG, DEBUG, INFO
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from OFS.Folder import Folder
 from zope.schema.fieldproperty import FieldProperty
@@ -28,7 +29,7 @@ from zope.app import zapi
 from zope.interface import implements
 from utils import uniqueId, makeId
 from Products.CPSMailAccess.interfaces import IMailBox, IMapping
-from Products.CPSMailAccess.mailfolder import MailFolder
+from Products.CPSMailAccess.mailfolder import MailFolder, manage_addMailFolder
 from Products.CPSMailAccess.baseconnection import ConnectionError, BAD_LOGIN, \
     NO_CONNECTOR
 from interfaces import IMailFolder, IMailMessage
@@ -72,13 +73,74 @@ class MailBox(MailFolder):
     def synchronize(self):
         """ see interface
         """
-        sub_folders = self.getMailMessages(list_folder=True, list_messages=False,
-            recursive=True)
+        # retrieving folder list from server
+        connector = self._getconnector()
+        server_directory = connector.list()
 
-        for sub_folder in sub_folders:
-            sub_folder._synchronizeFolder()
+        self._syncdirs(server_directory)
 
-        self._synchronizeFolder()
+        # now check messages within this folder
+        # XXXX to be done
+
+    def _syncdirs(self, server_directories = []):
+        """ syncing dirs
+        """
+        ### todo : make a smart algo that moves
+        ### folders instead of deleting them
+        ### and recreate
+
+        self.setSyncState(state=False, recursive=True)
+
+        for directory in server_directories:
+            # servers directory are delimited by dots
+            dirname = directory['Name']
+            dirattr = directory['Attributes']
+
+            dir_fullpath = dirname.split('.')
+
+            current_dir = self
+
+            size = len(dir_fullpath)
+            current = 1
+
+            for part in dir_fullpath :
+                # if path does not exists
+                # let's create it
+                # server_name is the key, not Id
+                # since folder can hav any caracters
+                founded = None
+                for id, item in current_dir.objectItems():
+                    if item.server_name == part:
+                        founded = item
+                        break
+
+                if founded is None:
+                    part_id = makeId(part)
+                    part_id = uniqueId(current_dir, part_id)
+                    manage_addMailFolder(current_dir, part_id, part)
+                    founded = getattr(current_dir, part_id)
+
+                current_dir.setSyncState(state=True)
+
+                current_dir = founded
+
+                if current == size:
+                    current_dir.setSyncState(state=True)
+                else:
+                    current +=1
+
+        self.setSyncState(state=True)
+
+        # cheking for orphan folders
+        folders = self.getMailMessages(list_folder=True, list_messages=False, recursive=True)
+
+        for folder in folders:
+            if not folder.sync_state:
+                # delete the folder (see for order problem later here)
+                parent_folder = folder.aq_inner.aq_parent
+                parent_folder.manage_delObjects([folder.getId()])
+            else:
+                self.setSyncState(state=False)
 
     def _getconnector(self):
         """get mail server connector
@@ -92,7 +154,6 @@ class MailBox(MailFolder):
             wm_tool.reloadPlugins()
 
         connection_params = self.connection_params
-
         connector = wm_tool.getConnection(connection_params)
 
         if connector is None:
