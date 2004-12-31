0a41d3d5f065ecab64da8cc14004830bbacc2e4c
tziade <devnull@localhost>
2004-12-31 14:33:05 +0000

    added cache implementation for message synchronization

--- mailbox.py
+++ mailbox.py
@@ -32,12 +32,25 @@ from Products.CPSMailAccess.interfaces import IMailBox, IMapping
 from Products.CPSMailAccess.mailfolder import MailFolder, manage_addMailFolder
 from Products.CPSMailAccess.baseconnection import ConnectionError, BAD_LOGIN, \
     NO_CONNECTOR
+from Products.CPSMailAccess.mailcache import MailCache
 from interfaces import IMailFolder, IMailMessage
 from Globals import InitializeClass
 from zope.schema.fieldproperty import FieldProperty
 
 ### see for CMF dependency here
 from Products.CMFCore.utils import getToolByName
+
+import thread
+
+lock = thread.allocate_lock()
+cache = MailCache()
+
+def getCache():
+    lock.acquire()
+    try:
+        return cache
+    finally:
+        lock.release()
 
 class MailBox(MailFolder):
     """ the main container
@@ -50,6 +63,7 @@ class MailBox(MailFolder):
     >>> IMapping.providedBy(f)
     True
     """
+    ### XXX see if "properties" are ok in zope3/five context
     meta_type = "CPSMailAccess Box"
     portal_type = meta_type
 
@@ -57,6 +71,9 @@ class MailBox(MailFolder):
 
     # see here for security
     connection_params = FieldProperty(IMailBox['connection_params'])
+
+    # one mail cache by mailbox
+    mail_cache = getCache()
 
     def __init__(self, uid=None, server_name='', **kw):
         MailFolder.__init__(self, uid, server_name, **kw)
@@ -70,24 +87,29 @@ class MailBox(MailFolder):
             self._getconnector()
             self.synchronize()
 
-    def synchronize(self):
+    def synchronize(self, REQUEST=None):
         """ see interface
         """
         # retrieving folder list from server
         connector = self._getconnector()
         server_directory = connector.list()
 
-        self._syncdirs(server_directory)
+        if not REQUEST:
+            self._syncdirs(server_directory)
+        else:
+            log = self._syncdirs(server_directory, True)
+            log.insert(0, 'synchronizing mailbox...')
+            log.append('... done')
+            logtext = '\n'.join(log)
+            return logtext
 
         # now check messages within this folder
         # XXXX to be done
 
-    def _syncdirs(self, server_directories = []):
+    def _syncdirs(self, server_directories = [], return_log=False):
         """ syncing dirs
         """
-        ### todo : make a smart algo that moves
-        ### folders instead of deleting them
-        ### and recreate
+        log = []
 
         self.setSyncState(state=False, recursive=True)
 
@@ -103,21 +125,29 @@ class MailBox(MailFolder):
             size = len(dir_fullpath)
             current = 1
 
+            relative_name = ''
+
             for part in dir_fullpath :
+
+                if relative_name == '':
+                    relative_name = part
+                else:
+                    relative_name = relative_name + '.' + part
+
                 # if path does not exists
                 # let's create it
                 # server_name is the key, not Id
                 # since folder can hav any caracters
                 founded = None
                 for id, item in current_dir.objectItems():
-                    if item.server_name == part:
+                    if item.server_name == relative_name:
                         founded = item
                         break
 
                 if founded is None:
                     part_id = makeId(part)
                     part_id = uniqueId(current_dir, part_id)
-                    manage_addMailFolder(current_dir, part_id, part)
+                    manage_addMailFolder(current_dir, part_id, relative_name)
                     founded = getattr(current_dir, part_id)
 
                 current_dir.setSyncState(state=True)
@@ -134,13 +164,52 @@ class MailBox(MailFolder):
         # cheking for orphan folders
         folders = self.getMailMessages(list_folder=True, list_messages=False, recursive=True)
 
+        # let's order folder : leaves at first
         for folder in folders:
             if not folder.sync_state:
+                # before deleting message that this folder hold,
+                # we want to put them in orphan list
+                for id, item in folder.objectItems():
+                    if IMailMessage.providedBy(item):
+                        if not self.mail_cache.inCache(item):
+                            self.mail_cache.putMessage(item)
+
                 # delete the folder (see for order problem later here)
                 parent_folder = folder.aq_inner.aq_parent
                 parent_folder.manage_delObjects([folder.getId()])
+
+        # now syncronizing messages
+        # this is done after to be able
+        # to retrieve orphan message thus accelerate the work
+        folders = self.getMailMessages(list_folder=True, list_messages=False, recursive=True)
+
+        for folder in folders:
+            # let's synchronize messages now
+            if return_log:
+                flog = folder._synchronizeFolder(True)
+                log.extend(flog)
             else:
-                self.setSyncState(state=False)
+                folder._synchronizeFolder()
+            folder.setSyncState(state=False)
+
+        # if there's stillelements in mail_cache,
+        # that means that thse messages are really gone
+        cache_size = len(self.mail_cache)
+        if cache_size > 0:
+            log.append('deleting %d messages' % cache_size)
+            self.mail_cache.emptyList()
+
+        if return_log:
+            return log
+
+    def _appendProperties(self, params = {}):
+        """ appends properties to param dictionnary
+        """
+        for property in self._properties:
+            id = property['id']
+            value = getattr(self, id)
+            params[id] = value
+        return params
 
     def _getconnector(self):
         """get mail server connector
@@ -154,6 +223,7 @@ class MailBox(MailFolder):
             wm_tool.reloadPlugins()
 
         connection_params = self.connection_params
+        connection_params = self._appendProperties(connection_params)
         connector = wm_tool.getConnection(connection_params)
 
         if connector is None:
@@ -179,6 +249,7 @@ class MailBox(MailFolder):
     def __setitem__(self, name, val):
         """ see interface
         """
+        found = False
         self.connection_params[name] = val
 
     def __delitem__(self, name):
@@ -205,6 +276,7 @@ class MailBox(MailFolder):
         """ see interface
         """
         return self.connection_params.items()
+
 
 """ classic Zope 2 interface for class registering
 """
