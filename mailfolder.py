48dd44440a923e41275f6d6cadde1b4ab1067770
tziade <devnull@localhost>
2004-12-31 14:32:01 +0000

    added message folder synchronization

--- mailfolder.py
+++ mailfolder.py
@@ -22,17 +22,20 @@
 
 A MailFolder contains mail messages and other mail folders.
 """
+from zLOG import LOG, DEBUG, INFO
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from OFS.Folder import Folder
 from Acquisition import aq_parent
 from zope.schema.fieldproperty import FieldProperty
 from zope.app import zapi
 from zope.interface import implements
-from utils import uniqueId, makeId
+from utils import uniqueId, makeId, md5Hash
 from Products.CPSMailAccess.mailmessage import MailMessage
 from interfaces import IMailFolder, IMailMessage, IMailBox
 from Globals import InitializeClass
 from Acquisition import aq_parent, aq_inner
+
+from Products.CPSMailAccess.baseconnection import ConnectionError
 
 class MailContainerError(Exception) :
     pass
@@ -68,6 +71,7 @@ class MailFolder(Folder):
         Folder.__init__(self, uid)
         self.mail_prefix = 'msg_'
         self.setServerName(server_name)
+        self.title = server_name
 
     def getMailBox(self):
         """See interfaces.IMailFolder
@@ -96,12 +100,20 @@ class MailFolder(Folder):
         for element in self.objectValues():
             for provider in providers :
                 if provider.providedBy(element):
-                    result.append(provider(element))
+                    # see ***
+                    result.insert(0, provider(element))
 
             if recursive:
                 if IMailFolder.providedBy(element):
-                    result.extend(element.getMailMessages(list_folder,
-                        list_messages, recursive))
+                    sub_folder = element.getMailMessages(list_folder,
+                        list_messages, recursive)
+
+                    # *** we rather insert messages in front
+                    # of the list, rather than extending it
+                    # for speed reasons so when elements are deleted,
+                    # a child gets found before its parent
+                    for element in sub_folder:
+                        result.insert(0, element)
 
         return result
 
@@ -153,15 +165,12 @@ class MailFolder(Folder):
         # typically resync
         self.server_name = server_name
 
-    def _addMessage(self, uid='', msg_key=''):
+    def _addMessage(self, msg_uid='', msg_key=''):
         """"See interfaces.IMailFolder
         """
-        if uid == '':
-            uid = uniqueId(self, self.mail_prefix)
-        else:
-            uid = makeId(uid)
+        uid = uniqueId(self, self.mail_prefix)
+        new_msg = MailMessage(uid, msg_uid, msg_key)
 
-        new_msg = MailMessage(uid, uid, msg_key)
         self._setObject(new_msg.getId(), new_msg)
         new_msg = self._getOb(new_msg.getId())
 
@@ -197,6 +206,19 @@ class MailFolder(Folder):
 
         return None
 
+    def findMessageByUid(self, msg_uid, recursive=True):
+        """ See interfaces.IMailFolder
+        """
+        # XXX see for caching here
+        message_list = self.getMailMessages(list_folder=False, list_messages=True, \
+            recursive=recursive)
+
+        for message in message_list:
+            if str(message.msg_uid) == str(msg_uid):
+                return message
+
+        return None
+
     def childFoldersCount(self):
         """ See interfaces.IMailFolder
         """
@@ -217,18 +239,85 @@ class MailFolder(Folder):
             for folder in folders:
                 folder.setSyncState(state, True)
 
-    def _synchronizeFolder(self):
+    def _createKey(self, uid, header):
+        """ creates unique key
+            for messages identification
+        """
+        elements = ''
+        for key in header.keys():
+            elements += str(header[key])
+        return md5Hash(str(uid)+elements)
+
+    def _synchronizeFolder(self, return_log=False):
         """ See interfaces.IMailFolder
         """
-        # XXXX using pass for unit testing
+        log = []
+        mailbox = self.getMailBox()
+        mail_cache = mailbox.mail_cache
         connector = self._getconnector()
-
-        server_content = connector.search(self.server_name, None,'ALL')
         zodb_messages = self.getMailMessages(False, True, False)
-
         # now syncing server_folder and current one
-        #raise str(zodb_messages)
+        for message in zodb_messages:
+            message.setSyncState(state=False)
 
+        try:
+            server_messages = connector.search(self.server_name, None,'ALL')
+        except ConnectionError:
+            # this will happen if the directory has been
+            # deleted form the server
+            server_messages = []
+
+        for message in server_messages:
+
+            # first of all, search message by uidwich is unique inside a folder
+            msg = self.findMessageByUid(message, recursive=False)
+
+            if msg is None:
+                # opti : Will do just one call
+                #msg_uid = connector.fetch(self.server_name, message, '(UID)')
+                #msg_uid = str(msg_uid['UID'])
+                msg_uid = str(message)
+
+                msg_headers = connector.fetch(self.server_name, message, '(RFC822.HEADER)')
+                msg_key = self._createKey(msg_uid, msg_headers)
+
+                # first of all, search message by key
+                #msg = self.findMessage(msg_key, recursive=False)
+
+                # check if the message is in the orphan list
+                if msg is None:
+                    msg = mail_cache.retrieveMessage(msg_key, remove=True)
+
+                    # the message has to be created
+                    if msg is None:
+                        log.append('adding message %s in %s' % (msg_uid, self.server_name))
+                        msg = self._addMessage(msg_uid, msg_key)
+
+                        # THIS IS A HACK,next step is to create
+                        # a message accordingly to the whol message string
+                        for header in msg_headers.keys():
+                            msg.setParam(header, msg_headers[header])
+                            if msg_headers.has_key('Subject'):
+                                msg.title = msg_headers['Subject']
+                    else:
+                        # was in cache, linking it to self
+                        log.append('moving message %s in %s' % (msg_uid, self.server_name))
+                        self._setObject(msg.getId(), msg)
+
+            if msg:
+                msg.setSyncState(state=True)
+
+        # now clear messages in zodb that appears to be
+        # deleted from the directory
+        # and put them in the cache
+        for message in zodb_messages:
+            if not message.sync_state:
+                if not mail_cache.inCache(message):
+                    log.append('adding %s message ni cache' % message.msg_uid)
+                    mail_cache.putMessage(message)
+                self.manage_delObjects([message.getId(),])
+        if return_log:
+            return log
 
     def checkMessages(self):
         """See interfaces.IMailFolder
