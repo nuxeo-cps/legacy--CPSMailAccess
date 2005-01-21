9937bb265e4d1561d6b7cf9bf38bad1253c396a6
tziade <devnull@localhost>
2005-01-21 17:03:17 +0000

    testing base mail view

---
+++ tests/test_basemailview.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- encoding: iso-8859-15 -*-
+# (C) Copyright 2004 Nuxeo SARL <http://nuxeo.com>
+# Author: Tarek Ziadé <tz@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+import unittest
+from zope.testing import doctest
+from Testing.ZopeTestCase import installProduct
+from Testing.ZopeTestCase import ZopeTestCase
+import os
+from Products.CPSMailAccess.basemailview import BaseMailMessageView
+
+installProduct('FiveTest')
+installProduct('Five')
+
+
+class BaseMailViewCase(ZopeTestCase):
+
+    def test_instance(self):
+        mv = BaseMailMessageView(None, None)
+        self.assertNotEquals(mv, None)
+
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(BaseMailViewCase),
+        doctest.DocTestSuite('Products.CPSMailAccess.basemailview'),
+        ))
