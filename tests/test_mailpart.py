642c3cd5701f90ce276527fff75da79a655ba7c1
tziade <devnull@localhost>
2005-01-26 18:12:22 +0000

    mailpart splitted from mailmessage

---
+++ tests/test_mailpart.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- encoding: iso-8859-15 -*-
+# (C) Copyright 2004 Nuxeo SARL <http://nuxeo.com>
+# Author: Tarek Ziadé <tz@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+import unittest
+from zope.testing import doctest
+from Testing.ZopeTestCase import installProduct
+from Testing.ZopeTestCase import ZopeTestCase
+import os
+from Products.CPSMailAccess.mailpart import MailPart
+from Products.CPSMailAccess.mailmessage import MailMessage
+
+installProduct('FiveTest')
+installProduct('Five')
+
+class MailPartTestCase(ZopeTestCase):
+
+    def test_instance(self):
+        part = MailPart('id', None, None)
+        self.assert_(part)
+
+    def test_getParent(self):
+        msg = MailMessage('id', 'sid')
+        part = MailPart('id', msg, None)
+        self.assertEquals(part.getParent(), msg)
+
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(MailPartTestCase),
+        doctest.DocTestSuite('Products.CPSMailAccess.mailpart'),
+        ))
