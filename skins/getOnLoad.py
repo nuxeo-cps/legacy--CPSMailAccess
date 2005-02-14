0d63d7421319d6dab1609b12346d8548bc1ddb5b
tziade <devnull@localhost>
2005-02-14 22:06:05 +0000

    using javascript to hilite search sresults

---
+++ skins/getOnLoad.py
@@ -0,0 +1,15 @@
+## Script (Python) "getOnLoad.py"
+#parameters=
+##$Id$
+""" return the body onload attribute content
+"""
+request = container.REQUEST
+
+if request.has_key('URL'):
+    URL = container.REQUEST['URL']
+    if URL is not None:
+        if (URL.endswith('search_form') or URL.endswith('advanced_search_form') or \
+                URL.endswith('searchMessage.html')):
+            return 'highlightSearchTerm();setFocus();'
+
+return 'setFocus();'
