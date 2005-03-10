c576b01d2deaf3314ef564ce8a40c2827574f97d
tziade <devnull@localhost>
2005-03-10 07:59:15 +0000

    readded unicodedecodeerror

--- mailrenderer.py
+++ mailrenderer.py
@@ -93,6 +93,9 @@ class MailRenderer:
             if not isinstance(result, unicode):
                 try:
                     result = unicode(result, pcharset)
+                except UnicodeDecodeError:
+                    # wrong  charset ?
+                    result = unicode(result, 'ISO-8859-15')
                 except LookupError:
                     # typically hapenning when wrong charset
                     # or when the codec was not installed in python codec
