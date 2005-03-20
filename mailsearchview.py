57c9515c575b4b4291503b3af0dca6a6035fa8b1
tziade <devnull@localhost>
2005-03-20 20:57:13 +0000

    added zemantic cataloging search

--- mailsearchview.py
+++ mailsearchview.py
@@ -23,14 +23,16 @@ from utils import getToolByName
 from mailexceptions import MailCatalogError
 from mailmessageview import MailMessageView
 
+from zemantic.public import *
+from zemantic.query import UnionChain
+
 class MailSearchView(BrowserView):
 
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
 
     def searchMessages(self, searchable_text):
-        """ search the catalog
-        """
+        """ search the catalog """
         # XXX encoding in iso8859-15 (today's CPS)
         searchable_text = searchable_text.strip().decode('ISO-8859-15')
         if searchable_text == '':
@@ -63,8 +65,7 @@ class MailSearchView(BrowserView):
         return results
 
     def traverseToObject(self, path):
-        """ transforms an url to its object
-        """
+        """ transforms an url to its object """
         # XXX needs refactoring
         mailbox = self.context
         path = path.split('/')
@@ -83,4 +84,62 @@ class MailSearchView(BrowserView):
             else:
                 return None
         return subject
+
+    def zemanticPredicateList(self):
+        """ retrieves all predicates """
+        mailbox = self.context
+        cat = mailbox._getZemanticCatalog()
+        list_ = list(cat.uniquePredicates())
+        list_.sort()
+        return list_
+
+    def zemanticSearchMessages(self, **kw):
+        """ zemantic query """
+        mailbox = self.context
+
+        i = 0
+        found = True
+        queries = []
+
+        while found:
+            rrelation = 'relation_%d' %  i
+            rvalue = 'value_%d' %  i
+
+            if kw.has_key(rrelation) and kw.has_key(rvalue):
+                relation = kw[rrelation]
+                # XXX encoding in iso8859-15 (today's CPS)
+                if not isinstance(relation, unicode):
+                    relation = relation.strip().decode('ISO-8859-15')
+
+                relation = relation.lower()
+
+                value = kw[rvalue]
+                if value.strip() == '':
+                    query = Query(Any, u'<%s>' % relation, Any)
+                else:
+                    if not isinstance(relation, unicode):
+                        value = value.strip().decode('ISO-8859-15')
+                    query = Query(Any, u'<%s>' % relation, u'"%s"' % value)
+
+                queries.append(query)
+                i +=1
+            else:
+                found = False
+
+        cat = mailbox._getZemanticCatalog()
+
+        # joining queries with an or statement
+        union = UnionChain()
+
+        for query in queries:
+            union.add(query)
+
+        results = cat.query(union)
+        results = list(results)
+
+        if results == [None]:
+            return []
+        else:
+            return [result.triple() for result in list(results)]
+            #return results
 
