094ae27df5b26a892d74a16078c38524c503ef3c
tziade <devnull@localhost>
2005-04-11 21:07:22 +0000

    from SIMPLIFIED branch

---
+++ mailfiltering.py
@@ -0,0 +1,280 @@
+# -*- coding: ISO-8859-15 -*-
+# Copyright (c) 2005 Nuxeo SARL <http://nuxeo.com>
+# Authors: Tarek Ziadé <tz@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+"""
+     MailFiltering knows how to do some actions on MailMessage
+     given some filter rules. a filter rule is a triple :
+
+         subject / condition / value / action / action parameter (optional)
+
+     filters are ordonnated
+
+     actions :
+
+         ° 1 : move to a specified folder
+         ° 2 : copy to a specified folder
+         ° 3 : label the message
+         ° 4 : set junk status
+
+    subjects :
+
+         ° subject
+         ° sender
+         ° date
+         ° To
+         ° Cc
+         ° To or CC
+
+    conditions :
+
+         ° 1: contains
+         ° 2: does not contains
+         ° 3: begin with
+         ° 4: ends with
+         ° 5: is
+         ° 6: isn't
+
+    MailFiltering knows also how to deal with mailboxes
+"""
+from UserList import UserList
+
+# zope 2 persistent classes
+from Globals import Persistent
+from ZODB.PersistentList import PersistentList
+
+# zope 3 persistent classes
+# can be used (same interface when switching to zope 3)
+#from persistent import Persistent
+#from persistent.list import PersistentList
+
+from zope.interface import implements
+
+from utils import md5Hash, getFolder, extractMailParts, decodeHeader
+from interfaces import IMailFilteringBackEnd
+
+class NormalBackEnd(UserList):
+    """
+    >>> f = NormalBackEnd()
+    >>> IMailFilteringBackEnd.providedBy(f)
+    True
+    """
+    implements(IMailFilteringBackEnd)
+
+    def countFilters(self):
+        return len(self)
+
+    def appendFilter(self, filter_):
+        self.append(filter_)
+
+    def deleteFilter(self, index):
+        del self[index]
+
+class MailFiltering:
+
+    def __init__(self, backend=None):
+        if backend is None:
+            self._filters = NormalBackEnd()
+        else:
+            self._filters = backend
+
+    def _listFilters(self):
+        return self._filters
+
+    def _countFilters(self):
+        return self._filters.countFilters()
+
+    def _appendFilter(self, filter_):
+        self._filters.appendFilter(filter_)
+
+    def _deleteFilter(self, index):
+        self._filters.deleteFilter(index)
+
+    def _findElement(self, subject, condition, value):
+        """ finds an entry """
+        i = 0
+        for element in self._listFilters():
+            if (element['subject'] == subject and
+                element['condition'] == condition and
+                element['value'] == value):
+                return i
+            else:
+                i += 1
+        return -1
+
+    def getFilterCount(self):
+        """ retrieve filter count """
+        return self._countFilters()
+
+    def getFilters(self):
+        """ retrieve filter count """
+        return self._listFilters()
+
+    def addFilter(self, subject, condition, value, action, action_param=None):
+        """ adds a filter to the list """
+        i = self._findElement(subject, condition, value)
+        if i == -1:
+            element = {}
+            element['subject'] = subject
+            element['condition'] = condition
+            element['value'] = value
+            element['action'] = action
+            element['action_param'] = action_param
+            self._appendFilter(element)
+
+    def delFilter(self, subject, condition, value, action, action_param=None):
+        """ deletes a filter from the list """
+        i = self._findElement(subject, condition, value)
+        if i != -1:
+            self._deleteFilter(i)
+
+    def deleteFilter(self, index):
+        """ direct index deletion """
+        self._deleteFilter(index)
+
+    def _matchCondition(self, value, condition, condition_value):
+        """ tests value with condition value
+
+        condition possible values :
+            ° 1: contains
+            ° 2: does not contains
+            ° 3: begin with
+            ° 4: ends with
+            ° 5: is
+            ° 6: isn't
+        """
+        value = value.lower().strip()
+
+        # XXX need to use message encoding here
+        value = decodeHeader(value)
+        if isinstance(value, str):
+            value = value.decode('ISO-8859-15')
+
+        condition_value = condition_value.lower().strip()
+        if isinstance(condition_value, str):
+            condition_value = condition_value.decode('ISO-8859-15')
+
+        if condition == 1:
+            return value.find(condition_value) != -1
+        elif condition == 2:
+            return value.find(condition_value) == -1
+        elif condition == 3:
+            return value.startswith(condition_value)
+        elif condition == 4:
+            return value.endswith(condition_value)
+        elif condition == 5:
+            if value == condition_value:
+                return True
+            values = extractMailParts(value)
+            return condition_value in values
+        elif condition == 6:
+            return value != condition_value
+        else:
+            return False
+
+    def _filterMatches(self, element, message):
+        """ see if a message matches a given filter """
+        header = element['subject']
+        header_values = message.getHeader(header)
+        if header_values == [] or header_values is None:
+            return False
+
+        # when mutli-value header, check for any
+        condition = element['condition']
+        condition_value = element['value']
+
+        for header_value in header_values:
+            if self._matchCondition(header_value, condition, condition_value):
+                return True
+        return False
+
+    def _actionFilter(self, element, message):
+        """ do an action on a message
+
+         actions :
+            ° 1 : move to a specified folder
+            ° 2 : copy to a specified folder
+            ° 3 : label the message
+            ° 4 : set junk status
+
+         return True if action has been done
+        """
+        action = element['action']
+        if action in (1, 2):
+            target_folder = element['action_param']
+            current_folder = message.getMailFolder()
+            if current_folder is not None:
+                if current_folder.server_name != target_folder:
+                    mailbox = current_folder.getMailBox()
+                    ob_target_folder = getFolder(mailbox, target_folder)
+
+                    if ob_target_folder is not None:
+                        if action == 1:
+                            current_folder.moveMessage(message.uid,
+                                                       ob_target_folder)
+                        else:
+                            current_folder.copyMessage(message.uid,
+                                                       ob_target_folder)
+                    return True
+        elif action == 3:
+            # todo : labeling has to be done on server side too
+            subject = message.getHeader('Subject')[0]
+            label = element['action_param']
+            new_subject = u'[%s] %s' %(label, subject)
+            message.setHeader('Subject', new_subject)
+            return True
+        elif action == 4:
+            message.isjunk = element['action_param']
+            return True
+        else:
+            pass
+
+        return False
+
+    def filterMessage(self, message):
+        """ apply an action """
+        for element in self._listFilters():
+            if self._filterMatches(element, message):
+                if self._actionFilter(element, message):
+                    break
+
+#
+# persistent Zope backend
+#
+class ZODBMailBackEnd(PersistentList):
+    """
+    >>> f = ZODBMailBackEnd()
+    >>> IMailFilteringBackEnd.providedBy(f)
+    True
+    """
+    implements(IMailFilteringBackEnd)
+
+    def countFilters(self):
+        return len(self)
+
+    def appendFilter(self, filter_):
+        self.append(filter_)
+
+    def deleteFilter(self, index):
+        del self[index]
+
+class ZMailFiltering(MailFiltering, Persistent):
+
+    def __init__(self, backend=None):
+        zodb_list = ZODBMailBackEnd()
+        MailFiltering.__init__(self, zodb_list)
---
+++ tests/test_mailfiltering.py
@@ -0,0 +1,195 @@
+#!/usr/bin/python
+# -*- encoding: iso-8859-15 -*-
+# (C) Copyright 2005 Nuxeo SARL <http://nuxeo.com>
+# Author: Tarek Ziadé <tz@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+import unittest
+from zope.testing import doctest
+from basetestcase import MailTestCase
+
+from Products.CPSMailAccess.mailfiltering import MailFiltering
+
+class MailFilteringTestCase(MailTestCase):
+
+    def __init__(self, methodName='runTest'):
+        MailTestCase.__init__(self, methodName)
+
+    def test_instanciate(self):
+        ob = MailFiltering()
+        self.assertNotEquals(ob, None)
+
+    def test_addFilter(self):
+        ob = MailFiltering()
+        ob.addFilter('subjet', 'condition', 'value', 'action', 'action_param')
+        self.assertEquals(len(ob._filters), 1)
+        ob.addFilter('subjet', 'condition', 'value', 'action2', 'action_param')
+        self.assertEquals(len(ob._filters), 1)
+        ob.addFilter('subjet', 'condition', 'valuze', 'action2', 'action_param')
+        ob.addFilter('subjet', 'condition', 'vaalue', 'action2', 'action_param')
+        self.assertEquals(len(ob._filters), 3)
+
+    def test_delFilter(self):
+        ob = MailFiltering()
+        ob.addFilter('subjet', 'condition', 'value', 'action', 'action_param')
+        ob.delFilter('subjet', 'condition', 'value', 'action', 'action_param')
+        self.assertEquals(len(ob._filters), 0)
+        ob.delFilter('subjet', 'condition', 'value', 'action', 'action_param')
+        self.assertEquals(len(ob._filters), 0)
+
+    def test_matchCondition(self):
+        ob = MailFiltering()
+        self.assert_(ob._matchCondition('i have a cool usbkey', 1, 'cool'))
+        self.assert_(not ob._matchCondition('i have a cool usbkey', 1, 'codol'))
+
+        self.assert_(not ob._matchCondition('i have a cool usbkey', 2, 'cool'))
+        self.assert_(ob._matchCondition('i have a cool usbkey', 2, 'codol'))
+
+        self.assert_(not ob._matchCondition('i have a cool usbkey', 3, 'we do'))
+        self.assert_(ob._matchCondition('i have a cool usbkey', 3, ' i have'))
+
+        self.assert_(not ob._matchCondition('i have a cool usbkey', 4, 'keys'))
+        self.assert_(ob._matchCondition('i have a cool usbkey', 4, 'key '))
+
+        self.assert_(ob._matchCondition('i have a cool usbkey', 5,
+                                            ' i have a cool usbkey  '))
+        self.assert_(ob._matchCondition('i have a cool usbkey', 5,
+                                        'i have a cool usbkey'))
+        self.assert_(not ob._matchCondition('i have a cool usbkey', 5, 'key '))
+
+        self.assert_(ob._matchCondition('i have a cool usbkey', 6, 'key '))
+        self.assert_(not ob._matchCondition('i have a cool usbkey', 6,
+                                            ' i have a cool usbkey  '))
+
+    def test_matchConditionMail(self):
+        # for extact matches when value looks like a mail
+        # we try to extract nude email or name
+        ob = MailFiltering()
+        self.assert_(ob._matchCondition('Tarek Ziadé <tz@nuxeo.com>', 5,
+                                        'tz@nuxeo.com'))
+
+        self.assert_(ob._matchCondition('Tarek Ziadé <tz@nuxeo.com>', 5,
+                                        'Tarek Ziadé'))
+
+    def headerEnconding(self):
+        ob = MailFiltering()
+        self.assert_(ob._matchCondition(u'Tarek Ziad\xe9', 5,
+                     '=?iso-8859-1?q?Tarek_Ziad=e9?='))
+
+    def test_filterMatches(self):
+        # now testing the real message filtering
+        ob = MailFiltering()
+
+        msg = self.getMailInstance(0)
+        msg2 = self.getMailInstance(1)
+        element = {}
+
+        element['subject'] = 'Subject'
+        element['condition'] = 1
+        element['value'] = 'test'
+
+        self.assert_(ob._filterMatches(element, msg))
+        self.assert_(not ob._filterMatches(element, msg2))
+
+        element['value'] = 'digest'
+
+        self.assert_(not ob._filterMatches(element, msg))
+        self.assert_(ob._filterMatches(element, msg2))
+
+        element['condition'] = 2
+        element['value'] = 'test'
+
+        self.assert_(not ob._filterMatches(element, msg))
+        self.assert_(ob._filterMatches(element, msg2))
+
+        element['value'] = 'digest'
+
+        self.assert_(ob._filterMatches(element, msg))
+        self.assert_(not ob._filterMatches(element, msg2))
+
+        element['condition'] = 3
+        element['value'] = 'This IS a'
+        self.assert_(ob._filterMatches(element, msg))
+        self.assert_(not ob._filterMatches(element, msg2))
+
+        element['condition'] = 4
+        element['value'] = 'message'
+        self.assert_(ob._filterMatches(element, msg))
+        self.assert_(not ob._filterMatches(element, msg2))
+
+    def test_actionFilter(self):
+        ob = MailFiltering()
+        msg = self.getMailInstance(0)
+        element = {}
+
+        self.assertEquals(msg.isjunk, 0)
+
+        # if subject contains test it's a junk
+        element['subject'] = 'Subject'
+        element['condition'] = 1
+        element['value'] = 'test'
+        element['action'] = 4
+        element['action_param'] = 1
+
+        ob._actionFilter(element, msg)
+        self.assertEquals(msg.isjunk, 1)
+
+        # if subject contains test it's a junk
+        # we append the label'TEST'
+        element['action'] = 3
+        element['action_param'] = 'TEST'
+        ob._actionFilter(element, msg)
+        self.assertEquals(msg.getHeader('Subject')[0],
+                          '[TEST] This is a test message')
+
+        # if subject contains test we move it to test folder
+        mailbox = self._getMailBox()
+        tests = mailbox._addFolder('tests', 'INBOX.tests')
+        msg1 = mailbox._addMessage('1', '1')
+        self.assertEquals(len(mailbox.objectIds()), 2)
+        self.assertEquals(len(tests.objectIds()), 0)
+
+        msg1.setHeader('Subject', 'This is a test message')
+
+        element['action'] = 1
+        element['action_param'] = 'INBOX.tests'
+
+        ob._actionFilter(element, msg1)
+
+        # checks that the message has been moved to tests folder
+        self.assertEquals(len(mailbox.objectIds()), 1)
+        self.assertEquals(len(tests.objectIds()), 1)
+
+        element['action'] = 2
+        element['action_param'] = 'INBOX.tests.tests'
+
+        self.assertEquals(len(tests.objectIds()), 1)
+
+         # checks that the message has been moved to ttests folder
+        ttests = tests._addFolder('tests', 'INBOX.tests.tests')
+        self.assertEquals(len(tests.objectIds()), 2)
+
+        element['action_param'] = 'INBOX.tests.tests'
+
+        ob._actionFilter(element, tests['.1'])
+
+        self.assertEquals(len(tests.objectIds()), 2)
+        self.assertEquals(len(ttests.objectIds()), 1)
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(MailFilteringTestCase),
+        doctest.DocTestSuite('Products.CPSMailAccess.mailfiltering'),
+        ))
