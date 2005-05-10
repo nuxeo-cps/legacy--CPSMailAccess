1afed94059491dd445405a3d4ec6b5d801fdb065
tziade <devnull@localhost>
2005-05-10 14:56:03 +0000

    redirection in background is a very bad idea

--- skins/background.py
+++ skins/background.py
@@ -1,7 +1,3 @@
 ##parameters=box_name, light=True
 # $Id$
-root = container.portal_url()
-resp = context.REQUEST.RESPONSE
-light = test(light, '1', '0')
-resp.redirect('%s/portal_webmail/%s/synchronize.html?light=%s' %(root, box_name, light))
-
+return container.portal_webmail[box_name].synchronize(light)
