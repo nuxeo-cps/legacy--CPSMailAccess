81fe4f1b42bf4f238e0e3b2b06b358ca2ded78fd
tziade <devnull@localhost>
2005-05-12 14:49:00 +0000

    redirecting to mailbox when possible

--- skins/background_synchro.py
+++ skins/background_synchro.py
@@ -1,6 +1,9 @@
 ##parameters=user,light=True
 # $Id$
-""" calls zasync if available or call direct synchro """
+""" calls zasync if available or call direct synchro
+    then redirect to INBOX if it already exists
+    or to root box
+"""
 box_name = 'box_%s' % user
 req = context.REQUEST
 
@@ -14,10 +17,17 @@ if hasattr(context, 'asynchronous_call_manager'):
     acm.putCall('zope_exec', '/', {},
                 'python:home.%s.portal_webmail.background("%s", %s)' \
                 % (root, box_name, light), {})
-    if req is not None:
-        root = container.portal_url()
-        psm = 'cps_synchronizing'
-        req.RESPONSE.redirect('%s/portal_webmail/%s/view?portal_status_message=%s' \
-                               % (root, box_name, psm))
 else:
     container.background(box_name, light)
+
+if req is not None:
+    root = container.portal_url()
+    psm = 'cps_synchronizing'
+    box = container.portal_webmail[box_name]
+
+    if hasattr(box, 'INBOX'):
+        page = '%s/portal_webmail/%s/INBOX' %(root, box_name)
+    else:
+        page = '%s/portal_webmail/%s' %(root, box_name)
+
+    req.RESPONSE.redirect('%s/view?portal_status_message=%s'  % (page, psm))
