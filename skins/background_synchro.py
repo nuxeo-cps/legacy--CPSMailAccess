2f8f23f05ee3539b6a4cb03b9c1f5cc4dbd451cd
tziade <devnull@localhost>
2005-05-17 12:36:59 +0000

    the light parameter is now an integer

--- skins/background_synchro.py
+++ skins/background_synchro.py
@@ -1,4 +1,4 @@
-##parameters=user,light=True
+##parameters=user,light=1
 # $Id$
 """ calls zasync if available or call direct synchro
     then redirect to INBOX if it already exists
@@ -9,14 +9,13 @@ req = context.REQUEST
 
 if hasattr(context, 'asynchronous_call_manager'):
     acm = context.asynchronous_call_manager
-    light = test(light, 'True', 'False')
     root = container.portal_url.getPortalPath()
     root = root.replace('/', '.')
     if root[0] == '.':
         root = root[1:]
     acm.putCall('zope_exec', '/', {},
                 'python:home.%s.portal_webmail.background("%s", %s)' \
-                % (root, box_name, light), {})
+                % (root, box_name, str(light)), {})
 else:
     container.background(box_name, light)
 
@@ -24,7 +23,6 @@ if req is not None:
     root = container.portal_url()
     psm = 'cps_synchronizing'
     box = container.portal_webmail[box_name]
-
     if hasattr(box, 'INBOX'):
         page = '%s/portal_webmail/%s/INBOX' %(root, box_name)
     else:
