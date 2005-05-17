529e1fb45356c7797805342bbd3584236dc28d86
tziade <devnull@localhost>
2005-05-17 12:35:35 +0000

    corrected name parameter

--- skins/background.py
+++ skins/background.py
@@ -1,3 +1,3 @@
-##parameters=box_name, light=True
+##parameters=box_name, light=1
 # $Id$
-return container.portal_webmail[box_name].synchronize(light)
+return container.portal_webmail[box_name].synchronize(light=light)
