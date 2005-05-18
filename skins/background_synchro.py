1c89657aeea0c69d319f8a83cc48679554417423
tziade <devnull@localhost>
2005-05-18 08:50:57 +0000

    changing message if failed

--- skins/background_synchro.py
+++ skins/background_synchro.py
@@ -3,11 +3,17 @@
 """ calls zasync if available or call direct synchro
     then redirect to INBOX if it already exists
     or to root box
+
+    XXXX needs to be put in mailtool class
 """
+# XXX executed as root when asynced,
+# need to change mails owner when they are created
 box_name = 'box_%s' % user
 req = context.REQUEST
+res = True
 
 if hasattr(context, 'asynchronous_call_manager'):
+    # XXX need to get feedback in case of failures
     acm = context.asynchronous_call_manager
     root = container.portal_url.getPortalPath()
     root = root.replace('/', '.')
@@ -17,11 +23,20 @@ if hasattr(context, 'asynchronous_call_manager'):
                 'python:home.%s.portal_webmail.background("%s", %s)' \
                 % (root, box_name, str(light)), {})
 else:
-    container.background(box_name, light)
+    try:
+        container.background(box_name, light)
+    # except ConnectionError: this will be available when code is put in
+    # mailtoool class
+    except:
+        res = False
 
 if req is not None:
     root = container.portal_url()
-    psm = 'cps_synchronizing'
+    if res:
+        psm = 'cps_synchronizing'
+    else:
+        psm = 'cpsma_failed_synchro'
+
     box = container.portal_webmail[box_name]
     if hasattr(box, 'INBOX'):
         page = '%s/portal_webmail/%s/INBOX' %(root, box_name)
