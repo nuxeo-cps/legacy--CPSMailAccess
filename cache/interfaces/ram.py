408a2bbb1912f633f432fbda8f4af7c6888a557a
tziade <devnull@localhost>
2006-01-02 14:04:45 +0000

    removing dependency for Zope 2.9b1

--- cache/interfaces/ram.py
+++ cache/interfaces/ram.py
@@ -20,9 +20,9 @@ __docformat__ = 'restructuredtext'
 from zope.interface import Attribute
 
 from cache.interfaces import ICache
-from zope.app.component.interfaces.registration import IRegisterable
+#from zope.app.component.interfaces.registration import IRegisterable
 
-class IRAMCache(ICache, IRegisterable):
+class IRAMCache(ICache):
     """Interface for the RAM Cache."""
 
     maxEntries = Attribute("""A maximum number of cached values.""")
